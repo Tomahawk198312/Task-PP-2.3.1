f02809efcfce7f9dfadfa620e92ac245bf4fafd9
Tomahawk198312 <129873302+Tomahawk198312@users.noreply.github.com>
2023-07-06 21:23:49 +0500

    Fix

--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -1,15 +1,20 @@
 package web.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import web.dao.UserDao;
-import web.dao.UserDaoImpl;
 import web.model.User;
 
 import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
-    UserDao userDao = new UserDaoImpl();
+    UserDao userDao;
+
+    @Autowired
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
 
     @Override
     public void saveUser(User user) {
